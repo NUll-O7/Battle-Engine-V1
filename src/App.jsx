a14651f54239b479793e2132e005e93570f110a3
NUll_O7 <dhruvsonimain@gmail.com>
2025-04-22 11:46:54 +0530

    Updated App component to properly integrate BattlePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { GameProvider } from './context/GameContext';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Game from './pages/Game';
-import Result from './pages/Result';
-import About from './pages/About';
+import { BattleProvider } from './context/BattleContext';
+import BattlePage from './pages/BattlePage';
 
 function App() {
   return (
-    <GameProvider>
+    <BattleProvider>
       <Router>
         <div className="min-h-screen flex flex-col bg-gray-50">
-          <Navbar />
-          <div className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/game" element={<Game />} />
-              <Route path="/result" element={<Result />} />
-              <Route path="/about" element={<About />} />
-            </Routes>
-          </div>
+          <Routes>
+            <Route path="/" element={<BattlePage />} />
+          </Routes>
         </div>
       </Router>
-    </GameProvider>
+    </BattleProvider>
   );
 }
 
